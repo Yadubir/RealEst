640b9d87d10786752759752d4e83407d47f888b3
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2024-12-21 00:19:15 +0530

    Initialized the Project

---
+++ app/(root)/(tabs)/profile.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const Profile = () => {
+    return (
+      <View>
+        <Text>sign-in</Text>
+      </View>
+    )
+}
+
+export default Profile;
