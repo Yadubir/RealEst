b6451d9e61d7566b06e9e3c90d3be668739b291d
Yadubir <yadubirjha2022@vitbhopal.ac.in>
2024-12-21 01:10:15 +0530

    Setup fonts and Inages

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <Text className="font-rubik text-3xl font-bold text-red-800">Welcome to ReState</Text>
       <Link href={"/sign-in"}> Sign-in</Link>
       <Link href={"/explore"}> Explore</Link>
       <Link href={"/profile"}> Profile</Link>
